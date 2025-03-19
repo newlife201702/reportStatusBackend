06adee884734af435f36cda9dcdbf2e198566a6e
yangzhicheng <1245204973@qq.com>
2025-03-19 16:09:46 +0800

    feat:修改端口号

--- index.js
+++ index.js
@@ -243,6 +243,6 @@ app.post('/viewOrders', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(2910, () => {
+  console.log('Server is running on port 2910');
 });
